ccfea646d7f8563943a843268f74950e983f0025
Youngmin <gosmain@naver.com>
2022-11-20 10:33:33 +0000

    생성후커밋

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h1>생성후테스트</h1>
     </div>
   );
 }
